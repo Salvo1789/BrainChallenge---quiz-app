1b134c54c03023c3f7833cc27f308524ac593c5b
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-09-05 12:20:05 +0200

    Structure and style initialized

---
+++ script.js
@@ -0,0 +1,75 @@
+const quizData = [
+    {
+        question: "In quale anno Cristoforo Colombo ha scoperto l'America?",
+        a: "1510",
+        b: "1492",
+        c: "1423",
+        d: "1476",
+        correct: "b"
+    },
+    {
+        question: "Quale casa automobilistica ha inventato il primo motore a due tempi?",
+        a: "Mercedes Benz",
+        b: "Fiat",
+        c: "Ford",
+        d: "Jaguar",
+        correct: "a"
+    },
+    {
+        question: "Qual'è l'isola più grande al mondo?",
+        a: "Far Oer",
+        b: "Australia",
+        c: "Madagascar",
+        d: "Groenlandia",
+        correct: "d"
+    },
+    {
+        question: "Che tipo di strumento è il sassofono?",
+        a: "Ad arco",
+        b: "A fiato",
+        c: "A percussione",
+        d: "A tastiera",
+        correct: "b"
+    },
+    {
+        question: "Chi è l'autore del libro \"Il ritratto di Dorian Gray\"?",
+        a: "Oscar Wilde",
+        b: "Victor Hugo",
+        c: "Mark Twain",
+        d: "William Shakespeare",
+        correct: "a"
+    },
+    {
+        question: "Il celebre brand informatico caratterizzato dal logo della mela morsicata.",
+        a: "Nokia",
+        b: "Samsung",
+        c: "Apple",
+        d: "IBM",
+        correct: "c"
+    },
+    {
+        question: "Che razza di cane è l'animale protagonista de \"Il commissario Rex\"?",
+        a: "Mastino Napoletano",
+        b: "Chihuahua",
+        c: "Doberman",
+        d: "Pastore tedesco",
+        correct: "d"
+    },
+    {
+        question: "Mediante quale unità di misura viene misurata l'intensità luminosa?",
+        a: "Watt",
+        b: "Pascal",
+        c: "Candela",
+        d: "Lumen",
+        correct: "c"
+    },
+    {
+        question: "Come si chiama il protagonista del famoso manga \"Dragon Ball\"?",
+        a: "Goku",
+        b: "Vegeta",
+        c: "Saitama",
+        d: "Zoro",
+        correct: "a"
+    }
+
+]
